78fff2efedf67fd00b1f4df47c2ea24c8386dae8
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 02:45:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,11 @@ def my_find(collection)
   i = 0 
   while i < collection.length 
     yield(collection[i])
+    if true 
+      return i 
+    else
+      yield(collection[i])
+    end
     i += 1
   end
 end
